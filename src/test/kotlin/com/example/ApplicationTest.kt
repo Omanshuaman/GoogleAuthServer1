604e58060fd8060c8250c6efe8f9d8f4ebc37b44
Omanshu Aman <omanshuaman@gmail.com>
2022-08-25 14:20:13 +0530

    Project update

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,17 +1,12 @@
 package com.example
 
-import com.example.domain.model.ApiRequest
-import com.example.domain.model.Endpoint
-import io.ktor.client.plugins.cookies.*
+import com.example.plugins.configureRouting
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ApplicationTest {
-
 }
